ba6545ff77544abe804214d1f5936b467340cc0b
benfiratkaya <benfiratkaya@gmail.com>
2021-03-04 15:44:54 +0300

    Fixed mobile UI problems

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -7,10 +7,10 @@ const Home = () => {
   return (
     <DefaultLayout>
       <div className="row">
-        <div className="col-4">
+        <div className="col-lg-4 order-2 order-lg-1">
           <Sidebar />
         </div>
-        <div className="col-8">
+        <div className="col-lg-8 order-1 order-lg-2 mb-4 mb-lg-0">
           <Preview />
         </div>
       </div>
